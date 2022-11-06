7a33566fef76c822bd39ab299267e4b4f3c729f4
BendyCodes <114787914+BendyCodes@users.noreply.github.com>
2022-11-06 13:45:10 -0300

    Delete pig_and_dragon directory

--- pig_and_dragon/src/assets/IconCoin.ts
+++
@@ -1,7 +0,0 @@
-class IconCoin extends Phaser.Group {
-    constructor() {
-        super(game);
-
-        this.add(new Phaser.Sprite(game, 0, 0, TEXTURE_ATLAS, 'IconCoin' + '0000'));
-    }
-}
--- pig_and_dragon/src/assets/Mc_x_mas_sale_icon.ts
+++
@@ -1,7 +0,0 @@
-class Mc_x_mas_sale_icon extends Phaser.Group {
-    constructor() {
-        super(game);
-
-        this.add(new Phaser.Sprite(game, 0, 0, TEXTURE_ATLAS, 'Mc_x_mas_sale_icon' + '0000'));
-    }
-}
